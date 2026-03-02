ef3f808f207523ce27f6f1d43feeb67b4c3b07cd
Rafael Zuquim <rzuquim@gmail.com>
2026-03-02 18:23:32 -0300

    unity hello world

---
+++ game_programming/unity_sandbox/Assets/Script/Hello.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public int SomeField;
+
+    void Start()
+    {
+        print("Hello from start");
+    }
+
+    void Update()
+    {
+        print("Hello from Update");
+    }
+}
