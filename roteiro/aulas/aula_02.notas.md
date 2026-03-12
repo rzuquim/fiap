dee88659e13cce6212ac54a4ca28c7c207cdaecf
Rafael Zuquim <rzuquim@gmail.com>
2026-03-12 14:40:38 -0300

    aula ux

---
+++ roteiro/aulas/aula_02.notas.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a10e427ff4ff77e017152005526250c4fba6fa93f7764ae6a26e7b8a78072236
+size 584
---
+++ ux/aula_1/scripts/main.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0c4bf2b815e80caa188b70938ccc7b3a0cd4c0f3c5dcd2114c301f7a811a7bae
+size 171
